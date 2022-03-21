10d2f438c591bde7202cd747dcf4ccf62954e523
Benroberts37 <benrobsemail@gmail.com>
2022-03-21 15:50:40 -0600

    I am attempting the functional component piece. I watched the lesson again and the review and was able to copy and paste over a lot of my content from appClass so we'll see how this works

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -2,7 +2,83 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default function AppFunctional(props) {
-  
+  URL = 'http://localhost:9000/api/result'
+  starterGrid = [[0, 0, 0], 
+                 [0, 1, 0], 
+                 [0, 0, 0]]
+
+  const [steps, setSteps] = useState(0)
+  const [grid, setGrid] = useState(starterGrid)
+  const [message, setMessage] = useState()
+  const [emailField, setEmailField] = useState()
+
+//copying and pasting over from AppClass
+  findBCoordinates = grid => {
+    for (let row = 0; row < grid.length; row++) {
+      for (let column = 0; column < grid.length; column++) {
+        if (grid[row][column]) return [row][column]
+      }
+    }
+  }
+
+  showCoordinates = grid => {
+    const [row,column] = this.findBCoordinates(grid)
+    return `(${column+1}, ${row+1})`
+  }
+
+  moveBSquare = (row, column) => {
+    this.setState({...this.state, grid: this.state.grid.map((rowArray, i) =>
+      rowArray.map((_, j) => (i === row && j === column) ? true : false)
+      ), 
+      steps: this.state.steps + 1,
+      message: null
+    })
+  }
+
+  directionToggle = direction => {
+    let [row, column] = this.findBCoordinates(this.state.grid)
+
+    if (direction === 'down' && row < 2) {
+      this.moveBSquare(row + 1, column)
+    } else if (direction === 'up' && row > 0){
+        this.moveBSquare(row + 1, column)
+    } else if (direction === 'right' && column < 2) {
+        this.moveBSquare(row, column + 1)
+    } else if (direction === 'left' && column > 0){
+        this.moveBSquare(row, column - 1);
+    } else {
+      this.setState({...this.state, message: `"You can't go ${direction}"`})
+    }
+    }
+
+    const reset = () => {
+      setTotalSteps(0)
+      setGrid(initialGrid)
+      setEmailField('')
+      setMessage()
+    }
+
+
+    const emailInput = e => {
+      const {value} = e.target
+      setEmailField(value)
+    }
+
+    onSubmit = e => {
+      e.preventDefault()
+      const [y,x] = showCoordinates(grid)
+      const payload = { "x": x + 1 , 
+                        "y": y + 1 , 
+                        "steps": steps,
+                        "email": emailField, }
+
+      axios.post(this.URL, payload)
+        .then(res => setMessage(res.data.message))
+        .catch(err => setMessage(res.data.message))
+        setEmailField('')
+    }
+
+
 
   return (
     <div id="wrapper">
