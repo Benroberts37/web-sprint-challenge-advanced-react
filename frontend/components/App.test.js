9801336d93457a9c223e5d611815b38282614b33
Benroberts37 <benrobsemail@gmail.com>
2022-03-22 07:35:42 -0600

    Cannot figure out the tests... going to re-do them

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,42 +1,52 @@
 // Write your tests here
-import React from 'react'
-import { render } from 'react-dom'
-import AppFunctional from './AppFunctional'
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import AppClass from './AppClass';
+import { render, screen } from '@testing-library/react';
+
+
 
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(false);
 })
 
-beforeEach(() => {
-  render(<AppFunctional/>)
-})
 
 test('renders the left toggle button', () => {
-  const leftToggle = screen.queryByText('LEFT')
-  expect(leftToggle).toBeVisible()
-  expect(leftToggle).toBeInTheDocument()
+  render(<AppClass />);
+
+  const leftToggle = screen.queryByText('LEFT');
+  expect(leftToggle).toBeVisible();
+  expect(leftToggle).toBeInTheDocument();
 })
 
 test('renders the right toggle button', () => {
-  const rightToggle = screen.queryByText('RIGHT')
-  expect(rightToggle).toBeVisible()
-  expect(rightToggle).toBeInTheDocument()
+  render(<AppClass />);
+
+  const rightToggle = screen.queryByText('RIGHT');
+  expect(rightToggle).toBeVisible();
+  expect(rightToggle).toBeInTheDocument();
 })
 
 test('The type your email box exists', () => {
-  const typeEmail = screen.getPlaceholderText('type email')
-  expect(typeEmail).toBeVisible()
-  expect(typeEmail).toBeInTheDocument()
+  render(<AppClass />);
+
+  const typeEmail = screen.getPlaceholderText('type email');
+  expect(typeEmail).toBeVisible();
+  expect(typeEmail).toBeInTheDocument();
 })
 
 test('Shows the user how many times they have moved', () => {
-  const movementCounter = screen.queryByText('You move 0 times')
-  expect(movementCounter).toBeVisible()
-  expect(movementCounter).toBeInTheDocument()
+  render(<AppClass />);
+
+  const movementCounter = screen.queryByText('You move 0 times');
+  expect(movementCounter).toBeVisible();
+  expect(movementCounter).toBeInTheDocument();
 })
 
 test('Shows the user what coordinates of the box they are on', () => {
-  const coordinates = screen.queryByText('Coordinates (2, 2)')
-  expect(coordinates).toBeVisible()
-  expect(coordinates).toBeInTheDocument()
+  render(<AppClass />);
+
+  const coordinates = screen.queryByText('Coordinates (2, 2)');
+  expect(coordinates).toBeVisible();
+  expect(coordinates).toBeInTheDocument();
 })
